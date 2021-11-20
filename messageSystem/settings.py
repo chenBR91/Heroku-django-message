3b692487b14e1287cbb8f07936a67347f3c5562a
Chen Brown <chenbrown91@gmail.com>
2021-11-20 21:26:59 +0200

    sqlight

--- messageSystem/settings.py
+++ messageSystem/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'a(y76sh8d(g0hke8f)^--m6h7y2y5uyp%chf75f%6j*$g%3lkf'
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['message-django-app.herokuapp.com']
+ALLOWED_HOSTS = ['message-django-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -78,16 +78,16 @@ WSGI_APPLICATION = 'messageSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
 '''
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -98,7 +98,7 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
