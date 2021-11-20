84349fe22df947415647e4ac3d83621b1fbb42aa
Chen Brown <chenbrown91@gmail.com>
2021-11-20 19:35:26 +0200

    sqlight3

--- messageSystem/settings.py
+++ messageSystem/settings.py
@@ -78,15 +78,16 @@ WSGI_APPLICATION = 'messageSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
 
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -97,7 +98,7 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
