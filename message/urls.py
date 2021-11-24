ee2281ef35c74f7ac9a2ac5df0f16e5ca4e31d28
Chen Brown <chenbrown91@gmail.com>
2021-11-24 10:51:12 +0200

    update message system

--- message/urls.py
+++ message/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path, include
+from django.urls import include, path
 from . import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('message', views.MessageViews)
+router.register('api/message', views.MessageViews)
 
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('message/', views.message_views, name='message_views'),
+    path('message/<username>/', views.message_views_specific_user, name='message_views_specific_user'),
+    path('readMessage/', views.read_message, name='read_message'),
+    path('deleteMessage/', views.delete_message, name='delete_message'),
+    path('loggedMessage/', views.logged_message, name='logged_message'),
 ]
--- message/views.py
+++ message/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework import viewsets, permissions
 from .models import Message
 from .srializers import MessageSerializers
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
@@ -9,5 +12,71 @@ from .srializers import MessageSerializers
 class MessageViews(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializers
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+# view all messages
+def message_views(request):
+    data = list(Message.objects.values())
+    return JsonResponse(data, safe=False)
+
+
+# view all message for username or show only read messages
+def message_views_specific_user(request, username):
+    # convert name to id
+    name = User.objects.get(username=username)
+    name_id = name.id
+
+    message = Message.objects.filter(sender=name_id)
+    try:
+        read = request.GET.get('read')
+        is_read = eval(read.title())  # convert str to boolean
+        unread_message = Message.objects.filter(sender=name_id, read=is_read)
+        print(unread_message)
+        unread_usr = list(unread_message.values())
+        return JsonResponse(unread_usr, safe=False)
+
+    except Exception as e:
+        print(e)
+
+    usr = list(message.values())
+    return JsonResponse(usr, safe=False)
+
+
+# view one message
+def read_message(request):
+    message_id = request.GET.get('message_id')
+    get_message = Message.objects.filter(pk=message_id)
+    message_content = list(get_message.values())
+    return JsonResponse(message_content, safe=False)
+
+
+# delete one message
+@csrf_exempt
+def delete_message(request):
+    message_id = request.GET.get('message_id')
+    get_user = Message.objects.filter(pk=message_id)
+    print('delete')
+    get_user.delete()
+    return JsonResponse({'status': '200'})
+
+
+# view all message only logged in users (is active)
+def logged_message(request):
+    user_logged = []
+    messages = Message.objects.all()
+    for message in messages:
+        if message.sender.is_active:
+            user_logged.append(message)
+
+    create_json = [{
+                    'id': element.id,
+                    'sender': element.sender.username,
+                    'receiver': element.receiver,
+                    'message': element.message,
+                    'subject': element.subject,
+                    'read': element.read
+                    } for element in user_logged]
+
+    return JsonResponse(create_json, safe=False)
 
